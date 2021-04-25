2131be8311eb069438565c6b5f3f20722655b9d6
nikita1803 <rainikita595@gmail.com>
2021-04-25 23:42:46 +0530

    Added who wins ot tie

--- coinSimulation.sh
+++ coinSimulation.sh
@@ -33,3 +33,39 @@ then
 else
 	  echo "Tail is winner because it comes".$countT."times"
 fi
+
+#UC-3
+
+i=1;
+countH=0;
+countT=0;
+output=0;
+while [ $i -le 21 ]
+do
+      result=$((RANDOM%2))
+      if [[ ${result} -eq 0 ]];
+      then
+         countH=`expr $countH + 1`
+         echo HEADS
+      elif [[ ${result} -eq 1 ]];
+      then
+         countT=`expr $countT + 1`
+         echo TAILS
+      fi
+   i=`expr $i + 1`
+done
+if [[ $countH -ge $countT ]];
+then
+    output=`expr $countH - $countT`
+    echo "Head is winner because it comes more than ".$output."times to the tail"
+elif [[ $countT -ge $countH ]];
+then
+	 output=`expr $countT - $countH`
+     echo "Tail is winner because it comes more than ".$output."times to the head"
+elif [[ $countH -eq $countT ]];
+then
+		echo "game is tie"
+else
+		echo ""
+fi
+
