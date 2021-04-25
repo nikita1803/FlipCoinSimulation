038ea2018f62109893d953e4ed23146b15b5ebe1
nikita1803 <rainikita595@gmail.com>
2021-04-26 00:11:07 +0530

    Game continues till the difference of minimum 2 points is achieved

--- coinSimulation.sh
+++ coinSimulation.sh
@@ -69,3 +69,41 @@ else
 		echo ""
 fi
 
+#UC-4
+function coinflip()
+{
+i=1;
+countH=0;
+countT=0;
+output=0;
+while [ $i -le 21 ]
+do
+      result=$((RANDOM%2))
+      if [[ ${result} -eq 0 ]];
+      then
+         countH=`expr $countH + 1`
+         echo HEADS
+      elif [[ ${result} -eq 1 ]];
+      then
+         countT=`expr $countT + 1`
+         echo TAILS
+      fi
+   i=`expr $i + 1`
+done
+}
+coinflip
+if [[ $countH -gt $countT ]];
+then
+    output=`expr $countH - $countT`
+    echo "Head is winner because it comes more than ".$output."times to the tail"
+elif [[ $countT -gt $countH ]];
+then
+    output=`expr $countT - $countH`
+     echo "Tail is winner because it comes more than ".$output."times to the head"
+else 
+
+      echo "game is tie"
+		coinflip
+
+fi
+
