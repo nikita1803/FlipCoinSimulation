c84dde51747f8732d524624dce44791f60c3ea85
nikita1803 <rainikita595@gmail.com>
2021-04-25 23:16:48 +0530

    show number of times head and tail has won

--- coinSimulation.sh
+++ coinSimulation.sh
@@ -7,3 +7,29 @@ if [[ ${Result} -eq 0 ]];then
 elif [[ ${Result} -eq 1 ]];then
 	echo TAILS
 fi
+
+#UC-2
+
+i=1;
+countH=0;
+countT=0;
+while [ $i -le 11 ]
+do 
+		result=$((RANDOM%2))
+		if [[ ${result} -eq 0 ]];
+		then
+			countH=`expr $countH + 1`
+			echo HEADS
+		elif [[ ${result} -eq 1 ]];
+		then
+ 			countT=`expr $countT + 1`
+			echo TAILS
+		fi
+	i=`expr $i + 1`
+done
+if [[ $countH -ge $countT ]];
+then
+	 echo "Head is winner because it comes".$countH."times"
+else
+	  echo "Tail is winner because it comes".$countT."times"
+fi
